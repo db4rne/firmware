d53e6de07ab47bc17bc78a5f6a583a156e2003a2
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-03-19 09:39:13 +0100

    update to HEAD of OpenWRT-master (Thu, 19 Mar 2018)
    5faa9556b1 mvebu: disable cpu idle on WRT1900ACv1
    e062bd8563 mac80211: avoid changing skb truesize in A-MSDU aggregation
    745d0e7f4b iproute2: fix hidden uint to uin64_t promotion in json_print
    b0b5d0aebb dnsmasq: bump to 2.79 release
    f4ea74abb6 curl: Update to 7.59
    9fc916a40c firmware-utils: mkdlinkfw: compile as gnu99
    8c88e23b99 lantiq: fix DM200 boot with fake uImage headers
    ac1c31c259 lantiq: fix AVM initramfs image
    26a9f01fc6 ar71xx: fix Fritz 300e initramfs image
    7a303fe5d1 scripts: add EVA ramboot script
    d13b05741a ramips: add support for TP-Link TL-WR902AC v3
    a5bd8de0bd ramips: add support for D-Link DWR-116-A1/2
    5323477184 firmware: add JBOOT based devices config extractor
    84c1b786b9 firmware-utils: add JBOOT bootloader image support
    d11aa1d4af ltq-vdsl-mei: Fix section mismatch
    5587b8f451 ltq-deu: Fix section mismatches
    becf58e080 e2fsprogs: fix InstallDev recipe
    e273860351 mtd: fix compile warnings
    5d3fbd5996 uboot-mxs: fix compile problems related to OpenSSL
    8cc22fad6a uboot-mxs: refresh patches
    8605ac87f8 mvebu: Disable internal RTC on Linksys devices
    dfe4de0287 mvebu: Add Solidrun RTC init patch.
    31717ec0ff mvebu: Backport RTC trimming support.
    14006efcf3 sunxi: update Xunlong Orange Pi Zero Plus dts file
    9899ffcfd3 kernel: bump 4.14 to 4.14.27
    895ea64a76 kernel: bump 4.4 to 4.4.121
    fa8e99c51d kernel: bump 4.9 to 4.9.87
    12d3d87a91 mwlwifi: Updated to upstream stable release
    96288dc139 generic: revert broken LED core patch
    49f3286bde openvpn: add config param verify_client_cert
    403260dbf7 ipq40xx: fix GL.iNet GL-B1300
    2796ab85ed ipq40xx: add support for Compex WPJ428
    1b773a46c2 build: Allow to change the FIT config section name
    dbdc26ba33 ramips: fix switch for Nexx devices

--- config.mk
+++ config.mk
@@ -3,6 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=5862f01ef71e7ec75394e877e74e781b7d08a4d3
+OPENWRT_COMMIT=5faa9556b1c10a4b21208615bddb7480401fa213
 SET_BUILDBOT=env
 MAKE_ARGS=
